bfe15e9bd74d460974c4c9bf6f4dea00ac0592d8
fred <popvlous@gmail.com>
2021-04-12 11:00:34 +0800

    1.獨立model 2.order的line items獨立出來

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from woocommerce import API
 from config import config_dict
+from model import db, Orders, OrdersLineItems
 
 pymysql.install_as_MySQLdb()
 
@@ -14,7 +15,6 @@ pymysql.install_as_MySQLdb()
 # collection = db.users
 # collection1 = db.inspections
 
-db = SQLAlchemy()
 
 # woocommerce api
 wcapi = API(
@@ -35,7 +35,7 @@ payload = {
 
 mode = 'Debug'
 app = Flask(__name__)
-#app.config["SERVER_NAME"] = 'test.com:5000'
+# app.config["SERVER_NAME"] = 'test.com:5000'
 
 if mode == 'Production':
     config = config_dict['Production']
@@ -52,7 +52,7 @@ if mode == 'Debug':
 db.init_app(app)
 
 
-#@app.route('/', subdomain="admin")
+# @app.route('/', subdomain="admin")
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -75,7 +75,8 @@ def show():
 
     # productlist = wcapi.get("products", params={"per_page": 20}).json()
 
-    response_productlist = requests.get('https://store.pyrarc.com/wp-json/wc/v3/products', data=payload, headers=my_headers)
+    response_productlist = requests.get('https://store.pyrarc.com/wp-json/wc/v3/products', data=payload,
+                                        headers=my_headers)
     productlist = json.loads(response_productlist.content.decode("utf-8").replace("'", '"'))
     print(productlist)
 
@@ -83,6 +84,7 @@ def show():
         'success': True,
         'msg': 'product list is {} '.format(productlist)
     })
+
 
 @app.route('/pcs/api/v1/customer', methods=['POST'])
 def showcustomers():
@@ -101,13 +103,27 @@ def showcustomers():
 
     # productlist = wcapi.get("products", params={"per_page": 20}).json()
 
-    response_customers = requests.get('https://store.pyrarc.com/wp-json/wc/v3/customers', data=payload, headers=my_headers)
+    response_customers = requests.get('https://store.pyrarc.com/wp-json/wc/v3/customers', data=payload,
+                                      headers=my_headers)
     customerlist = json.loads(response_customers.content.decode("utf-8").replace("'", '"'))
     print(customerlist)
 
     return jsonify({
         'success': True,
         'msg': 'product list is {} '.format(customerlist)
+    })
+
+
+@app.route('/pcs/api/v1/order')
+def showorder():
+    orders = Orders.query.filter().all()
+
+    json_format = json.dumps(orders)
+    print(json_format)
+
+    return jsonify({
+        'success': True,
+        'msg': 'orders list is {} '.format(json_format)
     })
 
 
@@ -123,12 +139,14 @@ def order_details():
     jwt_info = r.content.decode("utf-8").replace("'", '"')
     data = json.loads(jwt_info)
     my_headers = {'Authorization': "Bearer " + data['token']}
-    res_order_details = requests.get('https://store.pyrarc.com/wp-json/wc/v3/orders/' + str(req_id), data=payload, headers=my_headers)
+    res_order_details = requests.get('https://store.pyrarc.com/wp-json/wc/v3/orders/' + str(req_id), data=payload,
+                                     headers=my_headers)
     order_details = json.loads(res_order_details.content.decode("utf-8").replace("'", '"'))
     order_details_id = order_details['id']
     order_details_parent_id = order_details['parent_id']
     order_details_status = order_details['status']
     order_details_currency = order_details['currency']
+    order_details_version = order_details['version']
     order_details_date_created = order_details['date_created']
     order_details_date_modified = order_details['date_modified']
     order_details_total = order_details['total']
@@ -165,55 +183,107 @@ def order_details():
     # shipping_state = shipping['state']
     shipping_postcode = shipping['postcode']
     shipping_country = shipping['country']
+    # 保存line items
+    line_items = order_details['line_items']
+    # line_items_id = line_items['id']
+    # line_items_name = line_items['name']
+    # line_items_product_id = line_items['product_id']
+    # line_items_variation_id = line_items['variation_id']
+    # line_items_quantity = line_items['quantity']
+    # line_items_tax_class = line_items['tax_class']
+    # line_items_subtotal = line_items['subtotal']
+    # line_items_subtotal_tax = line_items['subtotal_tax']
+    # line_items_total = line_items['total']
+    # line_items_total_tax = line_items['total_tax']
+    # line_items_taxes = line_items['taxes']
+    # line_items_sku = line_items['sku']
+    # line_items_price = line_items['price']
+    # line_items_parent_name = line_items['parent_name']
 
-    # sql = "INSERT INTO Orders (order_id, parent_id, status, billing_first_name, billing_last_name)VALUES (" + str(
-    #     order_details_id) + ", " + "" + str(order_details_parent_id) + ", " + str(order_details_status) + ", " + str(
-    #     billing_first_name) + ", " + str(billing_last_name) + ")"
-    sql = 'INSERT INTO orders (order_id, parent_id, state, billing_first_name, billing_last_name, currency, date_created, date_modified, total, total_tax, payment_method, payment_method_title, transaction_id, customer_ip_address, created_via, customer_note, cart_hash, billing_company, billing_address_1 ,billing_address_2, billing_city, billing_state, billing_postcode, billing_country, billing_email, billing_phone, shipping_first_name, shipping_last_name, shipping_company, shipping_address_1, shipping_address_2, shipping_city, shipping_postcode, shipping_country )VALUES ({}, {}, {}, {}, {}, {}, NOW(), NOW(), {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})'.format(
-        order_details_id,
-        order_details_parent_id,
-        "'" + order_details_status + "'",
-        "'" + billing_first_name + "'",
-        "'" + billing_last_name + "'",
-        "'" + order_details_currency + "'",
-        # datetime.strptime(order_details_date_created, '%Y-%m-%dT%H:%M:%S'),
-        # datetime.strptime(order_details_date_modified, '%Y-%m-%dT%H:%M:%S'),
-        order_details_total,
-        order_details_total_tax,
-        "'" + order_details_payment_method + "'",
-        "'" + order_details_payment_method_title + "'",
-        "'" + order_details_transaction_id + "'",
-        "'" + order_details_customer_ip_address + "'",
-        "'" + order_details_created_via + "'",
-        "'" + order_details_customer_note + "'",
-        "'" + order_details_cart_hash + "'",
-        "'" + billing_company + "'",
-        "'" + billing_address_1 + "'",
-        "'" + billing_address_2 + "'",
-        "'" + billing_city + "'",
-        "'" + billing_state + "'",
-        "'" + billing_postcode + "'",
-        "'" + billing_country + "'",
-        "'" + billing_email + "'",
-        "'" + billing_phone + "'",
-        "'" + shipping_first_name + "'",
-        "'" + shipping_last_name + "'",
-        "'" + shipping_company + "'",
-        "'" + shipping_address_1 + "'",
-        "'" + shipping_address_2 + "'",
-        "'" + shipping_city + "'",
-        # "'" + shipping_state + "'",
-        "'" + shipping_postcode + "'",
-        "'" + shipping_country + "'"
-    )
-    db.engine.execute(sql)
-    print(jsonify(order_details))
-    return jsonify({
-        'success': True,
-        'msg': 'order record is create in blcokchain ',
-        'data': j_request_data
-    })
+    # 保存資料庫
+    order_info = Orders(order_details_id, order_details_parent_id, order_details_status, billing_first_name,
+                        billing_last_name, order_details_currency, order_details_version,order_details_total, order_details_total_tax,
+                        billing_company, billing_address_1, billing_address_2, billing_city, billing_state,
+                        billing_postcode, billing_country, billing_email, billing_phone, shipping_first_name,
+                        shipping_last_name, shipping_company, shipping_address_1, shipping_address_2, shipping_city,
+                        shipping_postcode, shipping_country, order_details_payment_method,
+                        order_details_payment_method_title, order_details_transaction_id,
+                        order_details_customer_ip_address, order_details_created_via, order_details_customer_note,
+                        order_details_cart_hash)
+    db.session.add(order_info)
+    db.session.commit()
+    # 保存資料庫
 
+    for line_item in line_items:
+        line_items_id = line_item['id']
+        line_items_name = line_item['name']
+        line_items_product_id = line_item['product_id']
+        line_items_variation_id = line_item['variation_id']
+        line_items_quantity = line_item['quantity']
+        line_items_tax_class = line_item['tax_class']
+        line_items_subtotal = line_item['subtotal']
+        line_items_subtotal_tax = line_item['subtotal_tax']
+        line_items_total = line_item['total']
+        line_items_total_tax = line_item['total_tax']
+        line_items_taxes = line_item['taxes']
+        line_items_sku = line_item['sku']
+        line_items_price = line_item['price']
+        line_items_parent_name = line_item['parent_name']
+        line_items_info = OrdersLineItems(order_details_id, line_items_id, line_items_name,
+                                 line_items_product_id, line_items_variation_id, line_items_quantity,
+                                 line_items_tax_class, line_items_subtotal, line_items_subtotal_tax, line_items_total,
+                                 line_items_total_tax, line_items_sku, line_items_price,
+                                 line_items_parent_name)
+        db.session.add(line_items_info)
+        db.session.commit()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=app.config['DEBUG'])
+        # sql = "INSERT INTO Orders (order_id, parent_id, status, billing_first_name, billing_last_name)VALUES (" + str(
+        #     order_details_id) + ", " + "" + str(order_details_parent_id) + ", " + str(order_details_status) + ", " + str(
+        #     billing_first_name) + ", " + str(billing_last_name) + ")"
+        # sql = 'INSERT INTO orders (order_id, parent_id, state, billing_first_name, billing_last_name, currency, date_created, date_modified, total, total_tax, payment_method, payment_method_title, transaction_id, customer_ip_address, created_via, customer_note, cart_hash, billing_company, billing_address_1 ,billing_address_2, billing_city, billing_state, billing_postcode, billing_country, billing_email, billing_phone, shipping_first_name, shipping_last_name, shipping_company, shipping_address_1, shipping_address_2, shipping_city, shipping_postcode, shipping_country )VALUES ({}, {}, {}, {}, {}, {}, NOW(), NOW(), {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})'.format(
+        #     order_details_id,
+        #     order_details_parent_id,
+        #     "'" + order_details_status + "'",
+        #     "'" + billing_first_name + "'",
+        #     "'" + billing_last_name + "'",
+        #     "'" + order_details_currency + "'",
+        #     # datetime.strptime(order_details_date_created, '%Y-%m-%dT%H:%M:%S'),
+        #     # datetime.strptime(order_details_date_modified, '%Y-%m-%dT%H:%M:%S'),
+        #     order_details_total,
+        #     order_details_total_tax,
+        #     "'" + order_details_payment_method + "'",
+        #     "'" + order_details_payment_method_title + "'",
+        #     "'" + order_details_transaction_id + "'",
+        #     "'" + order_details_customer_ip_address + "'",
+        #     "'" + order_details_created_via + "'",
+        #     "'" + order_details_customer_note + "'",
+        #     "'" + order_details_cart_hash + "'",
+        #     "'" + billing_company + "'",
+        #     "'" + billing_address_1 + "'",
+        #     "'" + billing_address_2 + "'",
+        #     "'" + billing_city + "'",
+        #     "'" + billing_state + "'",
+        #     "'" + billing_postcode + "'",
+        #     "'" + billing_country + "'",
+        #     "'" + billing_email + "'",
+        #     "'" + billing_phone + "'",
+        #     "'" + shipping_first_name + "'",
+        #     "'" + shipping_last_name + "'",
+        #     "'" + shipping_company + "'",
+        #     "'" + shipping_address_1 + "'",
+        #     "'" + shipping_address_2 + "'",
+        #     "'" + shipping_city + "'",
+        #     # "'" + shipping_state + "'",
+        #     "'" + shipping_postcode + "'",
+        #     "'" + shipping_country + "'"
+        # )
+        # db.engine.execute(sql)
+        print(jsonify(order_details))
+        return jsonify({
+            'success': True,
+            'msg': 'order record is create in blcokchain ',
+            'data': j_request_data
+        })
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000, debug=app.config['DEBUG'])
---
+++ model.py
@@ -0,0 +1,134 @@
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Orders(db.Model):
+    __table__name = 'orders'
+    # 設定 primary_key
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer)
+    parent_id = db.Column(db.Integer)
+    state = db.Column(db.String(50), nullable=True)
+    billing_first_name = db.Column(db.String(50), nullable=True)
+    billing_last_name = db.Column(db.String(50), nullable=True)
+    date_created = db.Column(db.DateTime, nullable=True)
+    date_modified = db.Column(db.DateTime, nullable=True)
+    total = db.Column(db.Integer, nullable=True)
+    total_tax = db.Column(db.Integer, nullable=True)
+    currency = db.Column(db.String(11), nullable=True)
+    version = db.Column(db.String(11), nullable=True)
+    customer_id = db.Column(db.Integer)
+    order_key = db.Column(db.String(100), nullable=True)
+    payment_method = db.Column(db.String(50), nullable=True)
+    payment_method_title = db.Column(db.String(50), nullable=True)
+    transaction_id = db.Column(db.String(50), nullable=True)
+    customer_ip_address = db.Column(db.String(50), nullable=True)
+    created_via = db.Column(db.String(50), nullable=True)
+    customer_note = db.Column(db.String(50), nullable=True)
+    date_completed = db.Column(db.String(50), nullable=True)
+    date_paid = db.Column(db.String(50), nullable=True)
+    cart_hash = db.Column(db.String(50), nullable=True)
+    billing_company = db.Column(db.String(50), nullable=True)
+    billing_address_1 = db.Column(db.String(500), nullable=True)
+    billing_address_2 = db.Column(db.String(500), nullable=True)
+    billing_city = db.Column(db.String(50), nullable=True)
+    billing_state = db.Column(db.String(50), nullable=True)
+    billing_postcode = db.Column(db.String(50), nullable=True)
+    billing_country = db.Column(db.String(50), nullable=True)
+    billing_email = db.Column(db.String(50), nullable=True)
+    billing_phone = db.Column(db.String(50), nullable=True)
+    shipping_first_name = db.Column(db.String(50), nullable=True)
+    shipping_last_name = db.Column(db.String(50), nullable=True)
+    shipping_company = db.Column(db.String(50), nullable=True)
+    shipping_address_1 = db.Column(db.String(500), nullable=True)
+    shipping_address_2 = db.Column(db.String(500), nullable=True)
+    shipping_city = db.Column(db.String(50), nullable=True)
+    # shipping_state = shipping['state']
+    shipping_postcode = db.Column(db.String(50), nullable=True)
+    shipping_country = db.Column(db.String(50), nullable=True)
+
+    def __init__(self, order_id, parent_id, state, billing_first_name, billing_last_name, currency, version, total,
+                 total_tax, billing_company, billing_address_1, billing_address_2, billing_city, billing_state,
+                 billing_postcode, billing_country, billing_email, billing_phone, shipping_first_name,
+                 shipping_last_name, shipping_company, shipping_address_1, shipping_address_2, shipping_city,
+                 shipping_postcode, shipping_country, payment_method, payment_method_title, transaction_id,
+                 customer_ip_address, created_via, customer_note, cart_hash):
+        self.order_id = order_id
+        self.parent_id = parent_id
+        self.state = state
+        self.billing_first_name = billing_first_name
+        self.billing_last_name = billing_last_name
+        self.currency = currency
+        self.version = version
+        self.total = total
+        self.total_tax = total_tax
+        self.date_created = datetime.utcnow()
+        self.date_modified = datetime.utcnow()
+        self.billing_company = billing_company
+        self.billing_address_1 = billing_address_1
+        self.billing_address_2 = billing_address_2
+        self.billing_city = billing_city
+        self.billing_state = billing_state
+        self.billing_postcode = billing_postcode
+        self.billing_country = billing_country
+        self.billing_email = billing_email
+        self.billing_phone = billing_phone
+        self.shipping_first_name = shipping_first_name
+        self.shipping_last_name = shipping_last_name
+        self.shipping_company = shipping_company
+        self.shipping_address_1 = shipping_address_1
+        self.shipping_address_2 = shipping_address_2
+        self.shipping_city = shipping_city
+        self.shipping_postcode = shipping_postcode
+        self.shipping_country = shipping_country
+        self.payment_method = payment_method
+        self.payment_method_title = payment_method_title
+        self.transaction_id = transaction_id
+        self.customer_ip_address = customer_ip_address
+        self.created_via = created_via,
+        self.customer_note = customer_note
+        self.cart_hash = cart_hash
+
+
+class OrdersLineItems(db.Model):
+    __table__name = 'orders_line_items'
+    # 設定 primary_key
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer)
+    line_items_id = db.Column(db.Integer)
+    line_items_name = db.Column(db.String(100), nullable=True)
+    line_items_product_id = db.Column(db.Integer)
+    line_items_variation_id = db.Column(db.Integer)
+    line_items_quantity = db.Column(db.Integer)
+    line_items_tax_class = db.Column(db.String(100), nullable=True)
+    line_items_subtotal = db.Column(db.Numeric(10, 6))
+    line_items_subtotal_tax = db.Column(db.Numeric(10, 6))
+    line_items_total = db.Column(db.Numeric(10, 6))
+    line_items_total_tax = db.Column(db.Numeric(10, 6))
+    line_items_taxes = db.Column(db.String(100), nullable=True)
+    line_items_sku = db.Column(db.String(50), nullable=True)
+    line_items_price = db.Column(db.Numeric(10, 6))
+    line_items_parent_name = db.Column(db.String(100), nullable=True)
+
+    def __init__(self, order_id, line_items_id, line_items_name, line_items_product_id,
+                 line_items_variation_id, line_items_quantity, line_items_tax_class, line_items_subtotal,
+                 line_items_subtotal_tax, line_items_total, line_items_total_tax, line_items_sku, line_items_price,
+                 line_items_parent_name):
+        self.order_id = order_id
+        self.line_items_id = line_items_id
+        self.line_items_name = line_items_name
+        self.line_items_product_id = line_items_product_id
+        self.line_items_variation_id = line_items_variation_id
+        self.line_items_quantity = line_items_quantity
+        self.line_items_tax_class = line_items_tax_class
+        self.line_items_subtotal = line_items_subtotal
+        self.line_items_subtotal_tax = line_items_subtotal_tax
+        self.line_items_total = line_items_total
+        self.line_items_total_tax = line_items_total_tax
+        #self.line_items_taxes = line_items_taxes
+        self.line_items_sku = line_items_sku
+        self.line_items_price = line_items_price
+        self.line_items_parent_name = line_items_parent_name
