62b244c0a2f798c48f9ba4f9480bd60b94eac47c
fred <popvlous@gmail.com>
2021-06-09 12:55:14 +0800

    1.add ineventory log

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ app.add_url_rule('/pcs/api/v1/inventory/deliveries', methods=['GET', 'POST'], vi
 # @app.route('/', subdomain="admin")
 @app.route('/')
 def hello_world():
-    return redirect('https://store.pyrarc.com')
+    return redirect('https://storeapi.pyrarc.com/backend/login')
 
 
 @app.route('/pcs/api/v1/product', methods=['GET', 'POST'])
@@ -632,6 +632,7 @@ def orderupdate():
             db.session.commit()
             inventory_bc_info = insertBlockChainInventory(inventory)
             print('inventory_create')
+            print(inventory)
             print(inventory_bc_info)
             inventory_tx_id = inventory_bc_info[1].decode("utf-8").replace("'", '"')
             #   更新tx
